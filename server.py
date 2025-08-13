c4e9d6bd7a86dfed2c5cb8a6395ff46f6734684f
Roman Z <roman-z@inbox.ru>
2025-08-13 16:21:51 +0500

    Make all pages, and GET and POST

--- server.py
+++ server.py
@@ -1,19 +1,43 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
+import json
+
+hostName = "localhost"
+serverPort = 10553
 
 
-def run_server():
-    server_address = ("", 8000)
-    serv_addr = server_address[1]
-    handler_class = SimpleHTTPRequestHandler
-    with HTTPServer(server_address, handler_class) as httpd:
-        print(f"Сервер запущен на локальном компьютере. Адрес порта {serv_addr}")
-        try:
-            httpd.serve_forever()
-        except KeyboardInterrupt:
-            pass
-        finally:
-            httpd.server_close()
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        with open('contacts.html', 'rb') as file:
+            page_content = file.read()
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(page_content)
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        received_data = json.loads(post_data.decode('utf-8'))
+
+        print("Полученные данные:", received_data)
+
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        response_body = {"message": "Данные получены"}
+        self.wfile.write(json.dumps(response_body).encode('utf-8'))
 
 
 if __name__ == "__main__":
-    run_server()
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print(f"Сервер запущен по адресу http://{hostName}:{serverPort}/")
+
+    try:
+         webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Сервер остановлен.")
