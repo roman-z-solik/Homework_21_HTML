303d78ea1a4bdcaa595d63fe4b5b5eb80e9048c1
Roman Z <roman-z@inbox.ru>
2025-08-13 15:23:12 +0500

    Make all pages

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+
 def run_server():
-    server_address = ('', 8000)
+    server_address = ("", 8000)
     serv_addr = server_address[1]
     handler_class = SimpleHTTPRequestHandler
     with HTTPServer(server_address, handler_class) as httpd:
-        print(f'Сервер запущен на локальном компьютере. Адрес порта {serv_addr}')
+        print(f"Сервер запущен на локальном компьютере. Адрес порта {serv_addr}")
         try:
             httpd.serve_forever()
         except KeyboardInterrupt:
@@ -13,5 +14,6 @@ def run_server():
         finally:
             httpd.server_close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     run_server()
